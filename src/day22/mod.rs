9781ffcab8749274185f7e1915d0d6e116b2f9ca
Sam Estep <sam@samestep.com>
2022-12-23 23:31:53 -0500

    Fix hidden day 22 bug

--- src/day22/mod.rs
+++ src/day22/mod.rs
@@ -70,51 +70,34 @@ enum Facing {
 
 use Facing::*;
 
-pub fn puzzle1(input: &str) -> usize {
-    let (grid, movements) = parse(input);
-    let rows: Vec<_> = grid
-        .iter()
-        .map(|row| {
-            let it = row.iter().enumerate().filter_map(|(j, t)| t.map(|_| j));
-            (it.clone().min(), it.max())
-        })
-        .collect();
-    let cols: Vec<_> = (0..grid[0].len())
-        .map(|j| {
-            let it = grid
-                .iter()
-                .enumerate()
-                .filter_map(|(i, row)| row[j].map(|_| i));
-            (it.clone().min(), it.max())
-        })
-        .collect();
-    let mut y = 0;
-    let mut x = rows[y].0.unwrap();
+fn get(grid: &[Vec<Option<bool>>], x: isize, y: isize) -> Option<bool> {
+    grid.get(y as usize)
+        .and_then(|row| row.get(x as usize).copied())
+        .flatten()
+}
+
+fn walk(
+    grid: &[Vec<Option<bool>>],
+    mut x: isize,
+    mut y: isize,
+    movements: &[Movement],
+    wrap: impl Fn(isize, isize, Facing) -> (isize, isize, Facing),
+) -> isize {
     let mut facing = R;
     for movement in movements {
         match movement {
             Go(n) => {
-                for _ in 0..n {
+                for _ in 0..*n {
                     let (mut i, mut j) = match facing {
                         R => (y, x + 1),
                         D => (y + 1, x),
                         L => (y, x - 1),
                         U => (y - 1, x),
                     };
-                    if grid
-                        .get(i)
-                        .and_then(|row| row.get(j).copied())
-                        .flatten()
-                        .is_none()
-                    {
-                        match facing {
-                            R => j = rows[y].0.unwrap(),
-                            D => i = cols[x].0.unwrap(),
-                            L => j = rows[y].1.unwrap(),
-                            U => i = cols[x].1.unwrap(),
+                    if get(grid, j, i).is_none() {
+                        (j, i, facing) = wrap(x, y, facing);
                         }
-                    }
-                    if let Some(Some(false)) = grid.get(i).and_then(|row| row.get(j)) {
+                    if let Some(false) = get(grid, j, i) {
                         (x, y) = (j, i);
                     }
                 }
@@ -141,6 +124,37 @@ pub fn puzzle1(input: &str) -> usize {
             L => 2,
             U => 3,
         }
+}
+
+pub fn puzzle1(input: &str) -> isize {
+    let (grid, movements) = parse(input);
+    let rows: Vec<_> = grid
+        .iter()
+        .map(|row| {
+            let it = row.iter().enumerate().filter_map(|(j, t)| t.map(|_| j));
+            (it.clone().min(), it.max())
+        })
+        .collect();
+    let cols: Vec<_> = (0..grid[0].len())
+        .map(|j| {
+            let it = grid
+                .iter()
+                .enumerate()
+                .filter_map(|(i, row)| row[j].map(|_| i));
+            (it.clone().min(), it.max())
+        })
+        .collect();
+    let y = 0;
+    let x = rows[y as usize].0.unwrap() as isize;
+    walk(&grid, x, y, &movements, |mut j, mut i, facing| {
+        match facing {
+            R => j = rows[i as usize].0.unwrap() as isize,
+            D => i = cols[j as usize].0.unwrap() as isize,
+            L => j = rows[i as usize].1.unwrap() as isize,
+            U => i = cols[j as usize].1.unwrap() as isize,
+        }
+        (j, i, facing)
+    })
 }
 
 #[cfg(test)]
